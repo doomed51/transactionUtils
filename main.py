af38cf642f9a84140d15108ccdbd2ab7038b9a39
Rachit <racshankar@deloitte.ca>
2021-10-19 18:18:22 -0400

    cleaning up comments and removing debug code, reformatted code into useable functions, making code more readable

--- main.py
+++ main.py
@@ -21,51 +21,66 @@ dropColumns = ['Activity Type', 'Settlement Date', 'Account Type', 'Account #', 
 
 transactionsDF.drop(columns = dropColumns, inplace=True)
 
-# find P&L from ANY and BA trades 
-#print("Transaction with:")
-#print(tickerArray)
-print("")
 
-#print(transactionsDF.loc[transactionsDF['Symbol'].isin(tickerArray)])
+#####
+# find the transaction history and 
+# net trading P&L for the passed in tickerArray
+##### 
+def findTransactionPandLByTicker(myTickerArray):
 
-print("")
-#print("Trade P&L")
-print("")
+    # find P&L from ANY and BA trades 
+    print("Transaction with:")
+    print(tickerArray)
+    print("")
+    print("Transaction History...")
 
-#print(
-# transactionsDF.loc[transactionsDF['Symbol']
-# .isin(tickerArray)]
-# .groupby('Symbol')['Net Amount']
-# .sum()
-# )
+    print(transactionsDF.loc[transactionsDF['Symbol'].isin(tickerArray)])
 
-#TODO find all symbols with net 0 holdings (i.e. no longer has open position), and figure out their P&L 
+    print("")
+    print("Trade P&L")
+    print("")
 
-# For the SYMBOLS where SUM OF QUANTITY = 0 over all trasactions
+    print(
+        transactionsDF.loc[transactionsDF['Symbol']
+        .isin(tickerArray)]
+        .groupby('Symbol')['Net Amount']
+        .sum()
+    )
+    print("")
 
-# create a df with symbol, sum of quantity, and sum of net amount
-sumQuantityOfAllSymbols = ( transactionsDF.groupby('Symbol')['Quantity', 'Net Amount']
-.sum()
-)
+#####
+# Find all symbols with net 0 holdings 
+# (i.e. no longer has open position), 
+# and figure out their P&L 
+######
+def findPandLOfClosedPositions():
+    # create a df with symbol, sum of quantity, and sum of net amount
+    sumQuantityOfAllSymbols = ( transactionsDF.groupby('Symbol')['Quantity', 'Net Amount']
+    .sum()
+    )
 
-# Select the Symbols that have no open positions 
-closedPositions = sumQuantityOfAllSymbols.loc[sumQuantityOfAllSymbols['Quantity'] == 0]
+    # Select the Symbols that have no open positions 
+    closedPositions = sumQuantityOfAllSymbols.loc[sumQuantityOfAllSymbols['Quantity'] == 0]
 
-#TODO for the closed positions, find ( net amount / sum of all buys * 100 ) 
-
-#print(sumQuantityOfAllSymbols.sort_values('Net Amount', ascending='true').head(15))
-
-# print closed positions
-print(closedPositions.sort_values('Net Amount', ascending=False).head(15))
-
-print("")
-print("###########")
-print("")
-
-# print the net P/L from ALL closed positions  
-print(closedPositions['Net Amount'].sum())
+    #TODO for the closed positions, find ( net amount / sum of all buys * 100 ) 
 
 
-#first col is symbol, second is sumof(quantity)
-# print the SUM OF ( NET AMOUNT )
-# print min(transaction date) and max(transaction date)
+    # print closed positions
+    print(closedPositions.sort_values('Net Amount', ascending=False).head(15))
+
+    print("")
+    print("###########")
+    print("")
+
+    # print the net P/L from ALL closed positions  
+    print("Net of all closed positions:") 
+    print(closedPositions['Net Amount'].sum())
+
+
+#####
+#END
+#####
+
+
+#findTransactionPandLByTicker(tickerArray)
+findPandLOfClosedPositions()
